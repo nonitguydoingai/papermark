ea4bf84417b9df24de350eed0bdfa3a96b5362d2
VishnuGabbula <2025viai@gmail.com>
2026-07-16 22:06:06 -0600

    Create confidential-view-section.tsx

---
+++ ee/features/permissions/components/confidential-view/confidential-view-section.tsx
@@ -0,0 +1,10 @@
+type Props = {
+  data: any;
+  setData: any;
+  isAllowed?: boolean;
+  handleUpgradeStateChange?: (...args: any[]) => void;
+};
+
+export default function ConfidentialViewSection(_props: Props) {
+  return null;
+}
